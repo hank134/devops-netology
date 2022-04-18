23d1b9cdd2f13bdaafa6c896fc231d80dccdd652
tim <zntimur@gmail.com>
2022-04-18 21:02:49 +0300

    414

--- terraform/virt73/main.tf
+++ terraform/virt73/main.tf
@@ -1,19 +1,19 @@
 # Provider
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#    }
-#  }
-#}
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
 provider "yandex" {
   service_account_key_file = "key.json"
-  cloud_id  = "b1ghn66scihrboe3sfhv"
-  folder_id = "b1goftamkelavkhtqfcd"
+  cloud_id  = ""
+  folder_id = ""
   zone = "ru-central1-a"
 }
-resource "yandex_storage_bucket" "test" {
-  access_key = "ajet2b8gtv8se4p8i65b"
-  secret_key = "YCPYvz4I7uvxzUVi-jYjrH_TbCq-K_YC84yYYuV1"
-  bucket = "ntlgtimbucket"
-}
+#resource "yandex_storage_bucket" "test" {
+#  access_key = "YCAJE1_c5ux10crvaLB1rQOzW"
+#  secret_key = "YCPYvz4I7uvxzUVi-jYjrH_TbCq-K_YC84yYYuV1"
+#  bucket = "ntlgtimbucket"
+#}
